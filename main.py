00805571f1f3f22db2cf2f6e085f7c19a3e3aa9e
Robert2547 <91036825+Robert2547@users.noreply.github.com>
2023-12-05 01:22:55 -0500

    Add GmailAPI

--- main.py
+++ main.py
@@ -1,27 +1,51 @@
-import imaplib
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
 
 
-host = 'imap.gmail.com'
-user = 'josephbenno2547@gmail.com'
-password = 'password'
+def main():
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
 
-# connect to host using SSL
-imap = imaplib.IMAP4_SSL(host)
+    try:
+        # Call the Gmail API
+        service = build("gmail", "v1", credentials=creds)
+        results = service.users().labels().list(userId="me").execute()
+        labels = results.get("labels", [])
 
-## login to server
-imap.login(user, password)
+        if not labels:
+            print("No labels found.")
+            return
+        print("Labels:")
+        for label in labels:
+            print(label["name"])
 
-imap.select('Inbox')
-
-tmp, data = imap.search(None, 'ALL')
-print(data)
-print(tmp)
-for num in data[0].split():
-    tmp, data = imap.fetch(num, '(RFC822)')
-    print('Message: {0}\n'.format(num))
-    print(data[0][1])
-    break
+    except HttpError as error:
+        # TODO(developer) - Handle errors from gmail API.
+        print(f"An error occurred: {error}")
 
 
-imap.close()
-imap.logout()
+if __name__ == "__main__":
+    main()
+# [END gmail_quickstart]
