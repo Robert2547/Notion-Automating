e1aae27f5d26a61a0de3f86da59fb22edcf8e35f
Robert2547 <91036825+Robert2547@users.noreply.github.com>
2023-12-11 12:39:28 -0500

    update gpt prompt

--- gptAI.py
+++ gptAI.py
@@ -17,18 +17,22 @@ else:
 
 
 def getGPT(email):
-    response = client.chat.completions.create( 
+    response = client.chat.completions.create(
         model="gpt-3.5-turbo-1106",
         messages=[
             {
                 "role": "system",
                 "content": """Pretend you are a consultants at analyzing email. You are given a email about software, determine whether this email is about new job posting, if so format the response in JSON format, such as: 
-"Job": [{
-     "Company": "string"
-     "Role": "string"
-     "Location": "string"
-    "URL": "string"
-}]""",
+{
+     Company:
+     Role:
+     Location:
+    URL:
+} if its anything else return the response in following JSON format and nothing else:
+{
+    Joblist: "No"
+}
+""",
             },
             {
                 "role": "user",
@@ -37,7 +41,7 @@ def getGPT(email):
         ],
     )
 
-     # Extract the response
+    # Extract the response
     generated_text = response.choices[0].message.content
 
     return generated_text
